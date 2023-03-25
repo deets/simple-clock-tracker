8f270f07ee51ae900f0e571f0742891e94c2686e
deets <deets@web.de>
2023-03-25 18:19:12 +0100

    Set board to PICO RP2040

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-arduino-cli compile --fqbn arduino:mbed_nano:nano33ble clock-tracker.ino
+CORE=arduino:mbed_nano:nano33ble
+CORE=arduino:mbed_rp2040:pico
+arduino-cli compile --fqbn "$CORE" clock-tracker.ino
--- flash.sh
+++ flash.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:mbed_nano:nano33ble clock-tracker.ino
+CORE=arduino:mbed_nano:nano33ble
+CORE=arduino:mbed_rp2040:pico
+arduino-cli upload -p /dev/ttyACM0 --fqbn $CORE clock-tracker.ino
