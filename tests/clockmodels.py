f336c8948fdd669227a741830936e7400c133ee4
Diez B. Roggisch <deets@clawstrider.local>
2023-03-30 16:23:27 +0200

    Added a piecewise linear clock model

--- tests/clockmodels.py
+++ tests/clockmodels.py
@@ -1,7 +1,8 @@
-class LinearClock:
+from itertools import cycle
 
-    def __init__(self, slope=1.0, start=0.0):
-        self._slope = slope
+class BaseClock:
+
+    def __init__(self, start):
         self._start = start
         self._first_host_timestamp = None
 
@@ -9,4 +10,30 @@ class LinearClock:
         if self._first_host_timestamp is None:
             self._first_host_timestamp = host_time
         diff = host_time - self._first_host_timestamp
-        return self._start + self._slope * diff
+        return self._start + self.compute(diff)
+
+
+class LinearClock(BaseClock):
+
+    def __init__(self, slope=1.0, start=0.0):
+        super().__init__(start)
+        self._slope = slope
+
+    def compute(self, diff):
+        return self._slope * diff
+
+
+class PiecewiseClock(BaseClock):
+
+    def __init__(self, pieces, start=0.0):
+        super().__init__(start)
+        self._pieces = pieces
+
+    def compute(self, diff):
+        value = 0.0
+        for length, slope in cycle(self._pieces):
+            if diff >= length:
+                diff -= length
+                value += length * slope
+            else:
+                return value + diff * slope
--- tests/test_clock_models.py
+++ tests/test_clock_models.py
@@ -1,13 +1,63 @@
-from clockmodels import LinearClock
+import pytest
+from clockmodels import LinearClock, PiecewiseClock
 
 
-def test_first_timestamp_yields_start():
-    linear_clock = LinearClock(slope=2.0, start=10.0)
-    assert(linear_clock.timestamp(1000.0) == linear_clock.timestamp(1000.0))
+def test_linearclock_first_timestamp_yields_start():
+    start = 10.0
+    linear_clock = LinearClock(slope=2.0, start=start)
+    assert linear_clock.timestamp(1000.0) == linear_clock.timestamp(1000.0)
+    assert linear_clock.timestamp(1000.0) == start
 
 
-def test_slope():
+def test_linearclock_slope():
     linear_clock = LinearClock(slope=2.0, start=10.0)
     first = linear_clock.timestamp(1000.0)
     second = linear_clock.timestamp(2000.0)
-    assert(second - first == 2000.0)
+    assert second - first == 2000.0
+
+
+def test_piecewise_first_timestamp_yields_start():
+    start = 23.0
+    piecewise_clock = PiecewiseClock(
+        # length, slope
+        [
+          (3.0, 2.0),
+          (2.0, 1.5)
+        ],
+        start=start,
+    )
+    assert piecewise_clock.timestamp(1000.0) == piecewise_clock.timestamp(1000.0)
+    assert piecewise_clock.timestamp(1000.0) == start
+
+
+def test_piecewise_slope_reproduced_within_first_segment():
+    start = 23.0
+    piecewise_clock = PiecewiseClock(
+        # length, slope
+        [
+          (3.0, 2.0),
+          (2.0, 1.5)
+        ],
+        start=start,
+    )
+    first = piecewise_clock.timestamp(0.0)
+    # less than the first segments length
+    second = piecewise_clock.timestamp(2.5)
+    assert (second - first) / 2.5 == 2.0
+
+
+def test_piecewise_slope_reproduced_within_second_segment():
+    start = 0.0
+    piecewise_clock = PiecewiseClock(
+        # length, slope
+        [
+          (3.0, 2.0),
+          (2.0, 1.5)
+        ],
+        start=start,
+    )
+    # seed the start timestamp
+    piecewise_clock.timestamp(0.0)
+    first = piecewise_clock.timestamp(3.1)
+    second = piecewise_clock.timestamp(3.1 + 1)
+    assert (second - first) / 1 == pytest.approx(1.5)
